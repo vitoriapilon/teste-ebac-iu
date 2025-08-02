4dc320d6b9d45b1094502798b72dc414c5761c52
Vitoria Pilon <vvpilon@gmail.com>
2025-08-02 07:52:44 +0200

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,12 +2,35 @@
 
 describe('Funcionalidade: Login', () =>{
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+afterEach(() => {
+    cy.screenshot()
+});
+
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('vvpilon@gmail.com')
         cy.get('#password').type('testeEBAC123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, vvpilon (não é vvpilon? Sair)')
     })
+
+it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
+        cy.get('#username').type('invalido@gmail.com')
+        cy.get('#password').type('testeEBAC123')
+        cy.get('.woocommerce-form > .button').click()
+        //cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.' )
+        cy.get('.woocommerce-error').should('exist')
+});
+
+it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
+        cy.get('#username').type('vvpilon@gmail.com')
+        cy.get('#password').type('testeEBAC')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail vvpilon@gmail.com está incorreta. Perdeu a senha?' )
+        cy.get('.woocommerce-error').should('exist')
+});
+
 })
